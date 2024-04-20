1af20fd3fb76b652dc775c349876655d12c0b89e
rorzor <roryaoc@gmail.com>
2024-04-20 22:09:04 +0800

    Basic UI based on https://www.youtube.com/watch?v=QU1pPzEGrqw up to camera

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame, sys
 from settings import *
+from debug import debug
+from level import Level
 
 class Game:
     def __init__(self):
@@ -7,19 +9,23 @@ class Game:
         # general setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        self.clock = pygame.time.Clock
+        pygame.display.set_caption("AgentRTS")
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
 
     def run(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit
+                    pygame.quit()
                     sys.exit()
 
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
 if __name__ == '__main__':
     game = Game()
-    game.run
+    game.run()
