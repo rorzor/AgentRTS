cd9411db83bc27822feaf13e5ddcebf235baf3a3
rorzor <roryaoc@gmail.com>
2024-04-26 21:23:36 +0800

    added player graphics and started weapon

--- level.py
+++ level.py
@@ -7,6 +7,7 @@ from player import Player
 from debug import debug
 from support import *
 from random import choice
+from weapon import Weapon
 
 class Level:
     def __init__(self):
@@ -52,8 +53,10 @@ class Level:
                             Tile((x,y),[self.visible_sprites, self.obstacle_sprites],'object',surf)
                         pass
         #        
-        self.player = Player((2000,1430),[self.visible_sprites],self.obstacle_sprites)
+        self.player = Player((2000,1430),[self.visible_sprites],self.obstacle_sprites,self.create_attack)
 
+    def create_attack(self):
+        Weapon(self.player,[self.visible_sprites])
 
     def run(self):
         # update and draw the game
